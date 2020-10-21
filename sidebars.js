21b9730a0145ffa4052ebc7d4db14dee2e941ec0
Ahmed Gamal <ahmedmgh67@gmail.com>
2020-10-21 02:00:02 +0200

    refactor
    Co-Authored-By: Karim Mohamed <37942217+KarimMohamed20@users.noreply.github.com>

--- sidebars.js
+++ sidebars.js
@@ -3,9 +3,9 @@ module.exports = {
     "Getting Started": ['getting-started', 
     // 'doc1', 'doc2', 'doc3'
   ],
-    Technologies: ['nodejs', 'php', 'laravel', 'golang', 'docker-compose', "asp.net", "python", 'static', 'rails'],
-    Databases: ['mongodb', 'mysql', 'postgresql'],
-    "Databases":["databases"],
+    Technologies: ['technologies/nodejs', 'technologies/php', 'technologies/laravel', 'technologies/golang', 'technologies/docker-compose', "technologies/asp.net", "technologies/python", 'technologies/static', 'technologies/rails'],
+    Databases: ['databases/mongodb', 'databases/mysql', 'databases/postgresql'],
+    "Databases":["databases/databases"],
     // Machines: ['creating-machines', "pro-machine", "ultimate-machine", 'machine-dashboard', 'file-manager'],
     Machines: ['machines/specs'],
     // "CLI Tool": ['cliinstalling', 'cliusage'],
